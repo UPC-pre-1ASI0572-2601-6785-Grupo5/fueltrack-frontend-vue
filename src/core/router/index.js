d698ce2a57ace9540ef805ce8ed838a6bb48e7a9
cristianipa7 <cristianipa7@gmail.com>
2026-07-01 00:28:05 -0500

    fix: change router to web hash history to avoid 404 on page refresh

--- src/core/router/index.js
+++ src/core/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '../../modules/iam/application/stores/authStore.js'
 
 const routes = [
@@ -79,7 +79,7 @@ const routes = [
 
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
